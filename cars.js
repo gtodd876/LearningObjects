a11a1831c0398920bf8648f23ff5cfc687ad3343
Todd Matthews <gtmatthews@gmail.com>
2017-05-28 07:33:01 -0400

    arrays and objects

--- cars.js
+++ cars.js
@@ -1,31 +1,35 @@
-var taxi =
+  function makeCar()
   {
-    make: "Webville Motors",
-    model: "Taxi",
-    year: 1955,
-    color: "yellow",
-    passenger: 4,
-    convertable: false,
-    mileage: 281341
-  };
+      var make = ["Chevy", "GM", "Fiat", "Webville Motors", "Trucker"];
+      var model =  ["Cadillac", "500", "Bel-air", "Taxi", "Torpedo"];
+      var year = [1955, 1957, 1948, 1954, 1961];
+      var color = ["red", "blue", "tan", "yellow", "white" ];
+      var convertible = [true, false];
 
-  function prequal(car)
-  {
-      if (car.mileage > 10000)
+      var rand1 = Math.floor(Math.random() * makes.length);
+      var rand2 = Math.floor(Math.random() * models.length);
+      var rand3 = Math.floor(Math.random() * years.length);
+      var rand4 = Math.floor(Math.random() * colors.length);
+      var rand5 = Math.floor(Math.random() * 5) + 1;
+      var rand6 = Math.floor(Math.random() * 2);
+
+      var car =
       {
-          return false;
-      } else if (car.year > 1960)
-      {
-          return false;
-      }
-    return true;
+        make: makes[rand1],
+        model: models[rand2],
+        year: years[rand3],
+        color: colors[rand4],
+        passengers: rand5,
+        convertible: convertible[rand6],
+        mileage: 0
+      };
+    return car;
   }
 
-  var worthALook = prequal(taxi);
-
-  if (worthALook)
+  function displayCar(car)
   {
-      console.log("You goot check out this " + taxi.make + " " + " " + taxi.model);
-  } else {
-      console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+      console.log("Your new car is a " + car.year + " " + car.make + " " + car.model);
   }
+
+  var carToSell = makeCar();
+  displayCar(carToSell);
